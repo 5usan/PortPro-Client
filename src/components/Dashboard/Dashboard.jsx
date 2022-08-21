85b60baee18d0e72333f6467c07d2e46cb5ab88a
5usan <susanshrestha2056@gmail.com>
2022-08-21 18:42:45 +0545

    implemented logout functionality

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { authActions } from "../../store/auth-slice";
+import Button from "../Button/Button";
 
 const Dashboard = () => {
+  const userState = useSelector((state) => state.auth.userData);
+  const user = userState ? userState : JSON.parse(localStorage.getItem("user"));
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div>
+      <div>
+        Welcome <b>{user.email}</b>, this is your dashboard
+      </div>
+      <Button buttonName="Logout" type="button" clickHandler={logoutHandler} />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/store/auth-slice.js
+++ src/store/auth-slice.js
@@ -3,27 +3,35 @@ import { postDataWithoutToken } from "../services/services";
 import { apiRoutes } from "../config/configRoute";
 
 const token = localStorage.getItem("token");
+const isLoggedIn = localStorage.getItem("isLoggedIn");
+const user = JSON.parse(localStorage.getItem("user"));
 const authSlice = createSlice({
   name: "Authentication",
   initialState: {
-    token: localStorage.getItem("token"),
-    isLoggedIn: localStorage.getItem("isLoggedIn"),
-    userData: "",
+    token: token ? token : "",
+    isLoggedIn: isLoggedIn ? isLoggedIn : "",
+    userData: user ? user : "",
     isSignUped: false,
   },
   reducers: {
     login(state, action) {
       const token = action.payload.token;
+      const user = action.payload.user;
+      console.log(user, "user");
       localStorage.setItem("token", token);
+      localStorage.setItem("user", JSON.stringify(user));
       localStorage.setItem("isLoggedIn", true);
       state.token = token;
       state.isLoggedIn = true;
+      state.userData = user;
     },
     logout(state) {
       localStorage.removeItem("token");
       localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("user");
       state.token = "";
       state.isLoggedIn = false;
+      state.userData = "";
     },
     signup(state, action) {
       state.isSignUped = true;
@@ -37,9 +45,8 @@ export const sentLoginData = (data) => {
       apiRoutes.login,
       JSON.stringify(data),
       (onSuccess) => {
-        const jwt = onSuccess.data.payload.data.token;
+        const token = onSuccess.data.payload.data.token;
         const user = onSuccess.data.payload.data;
-        const token = "Bearer " + jwt;
         dispatch(authActions.login({ token, user }));
       },
       (onFail) => {
